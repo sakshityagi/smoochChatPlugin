f84c020ec2f7078628fcc9ee7251e8cfdd3ea6f1
tabish.khan <tabish.khan@tothenew.com>
2016-01-22 12:31:06 +0530

    Added injected and added bootstrap in the content section.

--- control/settings/app.js
+++ control/settings/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function (angular,buildfire) {
-    angular.module('smoochChatPluginSettings', [])
+    angular.module('smoochChatPluginSettings', ['ui.bootstrap'])
         .controller('SettingsHomeCtrl',['$scope', 'Buildfire',function($scope, Buildfire){
             var SettingsHome = this;
             SettingsHome.helloWorld = 'Hello World';
