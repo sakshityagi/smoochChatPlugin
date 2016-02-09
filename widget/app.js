05fee27fd4394c7a033334838d1eeea96065a828
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-02-09 17:33:36 +0530

    Changes made for pivotal issue: #112972997

--- widget/app.js
+++ widget/app.js
@@ -12,6 +12,7 @@
 
         /*Init method call, it will bring all the pre saved data*/
         WidgetHome.init = function () {
+            WidgetHome.updateColorTheme = false;
           WidgetHome.success = function (result) {
             console.info('init success result:', result);
             if (result) {
@@ -86,57 +87,61 @@
         WidgetHome.onUpdateCallback = function (event) {
           setTimeout(function () {
             if (event) {
-              WidgetHome.data = event.data;
-              WidgetHome.apiKey = WidgetHome.data.settings.apiKey;
-              Buildfire.spinner.show();
-              var initObj = {
-                  appToken: WidgetHome.apiKey,
-                  customText: {headerText: WidgetHome.data.settings.headerText || "How can we help?"}
-              };
-              if(WidgetHome.instanceId) {
-                  initObj.userId = WidgetHome.instanceId;
-              }
-              var smoochApp = Smooch.init(initObj);
-              smoochApp.then(function (res) {
-                console.log("??????????????onUpdate", res);
-                Buildfire.spinner.hide();
-                if (res && res._id) {
-                  WidgetHome.invalidApiKey = false;
-                  $('#sk-header').click(function(event){
-                    event.stopPropagation();
-                  });
-                    $("#sk-footer form a").bind('taphold', function(event) {
-                        event.preventDefault();
-                    });
-                  $('.send').off('click');
-                  if(WidgetHome.data.design == undefined){
-                    WidgetHome.data.design = {};
-
-                    WidgetHome.data.design.color = "5d8aa8";
-                  }
-                  WidgetHome.className = "color-"+WidgetHome.data.design.color ||"5d8aa8";
-                  $('#sk-holder #sk-container #sk-conversation .sk-row.sk-right-row .sk-msg').removeClass(function (index, css) {
-                    return (css.match (/\color-\S+/g) || []).join(' ');
-                  });
-                  $('#sk-holder #sk-container #sk-conversation .sk-row.sk-right-row .sk-msg').addClass(WidgetHome.className);
-                  Smooch.open();
-                  $scope.$digest();
+                WidgetHome.data = event.data;
+                WidgetHome.apiKey = WidgetHome.data.settings.apiKey;
+                Buildfire.spinner.show();
+                var initObj = {
+                    appToken: WidgetHome.apiKey,
+                    customText: {headerText: WidgetHome.data.settings.headerText || "How can we help?"}
+                };
+                if (WidgetHome.instanceId) {
+                    initObj.userId = WidgetHome.instanceId;
                 }
-                Smooch.on('message:sent', function(message) {
-                  WidgetHome.className = "color-"+WidgetHome.data.design.color;
-                  setTimeout(function(){
-                    $('#sk-holder #sk-container #sk-conversation .sk-row.sk-right-row .sk-msg').addClass(WidgetHome.className);
+                if (!WidgetHome.updateColorTheme) {
+                    WidgetHome.updateColorTheme = true;
+                    var smoochApp = Smooch.init(initObj);
+                    smoochApp.then(function (res) {
+                        console.log("??????????????onUpdate", res);
+                        Buildfire.spinner.hide();
+                        WidgetHome.updateColorTheme = false;
+                        if (res && res._id) {
+                            WidgetHome.invalidApiKey = false;
+                            $('#sk-header').click(function (event) {
+                                event.stopPropagation();
+                            });
+                            $("#sk-footer form a").bind('taphold', function (event) {
+                                event.preventDefault();
+                            });
+                            $('.send').off('click');
+                            if (WidgetHome.data.design == undefined) {
+                                WidgetHome.data.design = {};
 
-                  },0);
-                });
-              }, function (err) {
-                WidgetHome.invalidApiKey = true;
-                console.log("??????????????e", err);
-                Buildfire.spinner.hide();
-                $scope.$digest();
-                Smooch.destroy();
-              });
-              $scope.$digest();
+                                WidgetHome.data.design.color = "5d8aa8";
+                            }
+                            WidgetHome.className = "color-" + WidgetHome.data.design.color || "5d8aa8";
+                            $('#sk-holder #sk-container #sk-conversation .sk-row.sk-right-row .sk-msg').removeClass(function (index, css) {
+                                return (css.match(/\color-\S+/g) || []).join(' ');
+                            });
+                            $('#sk-holder #sk-container #sk-conversation .sk-row.sk-right-row .sk-msg').addClass(WidgetHome.className);
+                            Smooch.open();
+                            $scope.$digest();
+                        }
+                        Smooch.on('message:sent', function (message) {
+                            WidgetHome.className = "color-" + WidgetHome.data.design.color;
+                            setTimeout(function () {
+                                $('#sk-holder #sk-container #sk-conversation .sk-row.sk-right-row .sk-msg').addClass(WidgetHome.className);
+
+                            }, 0);
+                        });
+                    }, function (err) {
+                        WidgetHome.invalidApiKey = true;
+                        console.log("??????????????e", err);
+                        Buildfire.spinner.hide();
+                        $scope.$digest();
+                        Smooch.destroy();
+                    });
+                    $scope.$digest();
+                }
             }
           }, 0);
         };
